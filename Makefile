adbd81c71eda512d7f1c8afd5be92ba8fc9afaeb
Rafael <araujorafael9201@gmail.com>
2023-01-18 15:02:10 -0300

    Blank Screen

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CC = g++
+ARGS = -lsfml-graphics -lsfml-window -lsfml-system
 
 main: main.o
-	$(CC) -o main main.o
+	$(CC) -o main main.o -lsfml-graphics $(ARGS)
 
 main.o: main.cpp
-	$(CC) -c main.cpp
+	$(CC) -c main.cpp $(ARGS) 
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Chess!" << endl;
+    sf::VideoMode display(800, 800);
+    sf::RenderWindow window(display, "Chess");
+
+    while (window.isOpen()) {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+
+        window.clear();
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
